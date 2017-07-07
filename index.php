0a214b858878e5ac58c20d4c61978a016b079511
CoolSquid <mail@coolsquid.me>
2017-07-07 18:11:03 +0200

    Fix intendation

--- index.php
+++ index.php
@@ -7,39 +7,39 @@
 	</head>
 	<body>
         <div class="content">
-                <h1>Minecraft mods</h1>
-				<h2>React</h2>
-				<p>React is a mod that allows server admins and modpack authors to execute configurable actions whenever an event occurs.</p>
-				<p><span class="link">GitHub:</span> <a href="https://github.com/coolsquid/React">https://github.com/coolsquid/React</a></p>
-				<p><span class="link">CurseForge:</span> <a href="https://minecraft.curseforge.com/projects/react">https://minecraft.curseforge.com/projects/react</a></p>
+			<h1>Minecraft mods</h1>
+			<h2>React</h2>
+			<p>React is a mod that allows server admins and modpack authors to execute configurable actions whenever an event occurs.</p>
+			<p><span class="link">GitHub:</span> <a href="https://github.com/coolsquid/React">https://github.com/coolsquid/React</a></p>
+			<p><span class="link">CurseForge:</span> <a href="https://minecraft.curseforge.com/projects/react">https://minecraft.curseforge.com/projects/react</a></p>
 
-				<h2>PackGuard</h2>
-				<p>React is a mod that monitors the mod list for unexpected changes, notifying the user if anything has changed, as well as putting warnings in logs and crash reports. This is intended to help modpack authors avoid bug reports from modified packs.</p>
-				<p><span class="link">GitHub:</span> <a href="https://github.com/coolsquid/PackGuard">https://github.com/coolsquid/PackGuard</a></p>
-				<p><span class="link">CurseForge:</span> <a href="https://minecraft.curseforge.com/projects/packguard">https://minecraft.curseforge.com/projects/packguard</a></p>
+			<h2>PackGuard</h2>
+			<p>React is a mod that monitors the mod list for unexpected changes, notifying the user if anything has changed, as well as putting warnings in logs and crash reports. This is intended to help modpack authors avoid bug reports from modified packs.</p>
+			<p><span class="link">GitHub:</span> <a href="https://github.com/coolsquid/PackGuard">https://github.com/coolsquid/PackGuard</a></p>
+			<p><span class="link">CurseForge:</span> <a href="https://minecraft.curseforge.com/projects/packguard">https://minecraft.curseforge.com/projects/packguard</a></p>
 
-				<h2>MiscTweaks</h2>
-				<p>MiscTweaks is my random-options-for-random-things mod. It features a broad variety of config options for Vanilla, from disabling sleeps to removing lava pockets in the nether. Many of the options are primarily intended for modpack authors.</p>
-				<p><span class="link">GitHub:</span> <a href="https://github.com/coolsquid/MiscTweaks">https://github.com/coolsquid/MiscTweaks</a></p>
-				<p><span class="link">CurseForge:</span> <a href="https://minecraft.curseforge.com/projects/misctweaks_">https://minecraft.curseforge.com/projects/misctweaks_</a></p>
+			<h2>MiscTweaks</h2>
+			<p>MiscTweaks is my random-options-for-random-things mod. It features a broad variety of config options for Vanilla, from disabling sleeps to removing lava pockets in the nether. Many of the options are primarily intended for modpack authors.</p>
+			<p><span class="link">GitHub:</span> <a href="https://github.com/coolsquid/MiscTweaks">https://github.com/coolsquid/MiscTweaks</a></p>
+			<p><span class="link">CurseForge:</span> <a href="https://minecraft.curseforge.com/projects/misctweaks_">https://minecraft.curseforge.com/projects/misctweaks_</a></p>
 
-				<h2>EnhancedChat</h2>
-				<p>EnhancedChat is a mod that adds miscellaneous chat-related improvements, without overhauling the entire mechanism.</p>
-				<p><span class="link">GitHub:</span> <a href="https://github.com/coolsquid/EnhancedChat">https://github.com/coolsquid/EnhancedChat</a></p>
-				<p><span class="link">CurseForge:</span> <a href="https://minecraft.curseforge.com/projects/enhancedchat">https://minecraft.curseforge.com/projects/enhancedchat</a></p>
+			<h2>EnhancedChat</h2>
+			<p>EnhancedChat is a mod that adds miscellaneous chat-related improvements, without overhauling the entire mechanism.</p>
+			<p><span class="link">GitHub:</span> <a href="https://github.com/coolsquid/EnhancedChat">https://github.com/coolsquid/EnhancedChat</a></p>
+			<p><span class="link">CurseForge:</span> <a href="https://minecraft.curseforge.com/projects/enhancedchat">https://minecraft.curseforge.com/projects/enhancedchat</a></p>
 
-				<h2>Creepers Fire</h2>
-				<p>Creepers Fire is a mod that makes creepers explode when set on fire. It was originally created by strikingwolf for Minecraft 1.7.10, and updated to later versions by me.</p>
-				<p><span class="link">GitHub:</span> <a href="https://github.com/coolsquid/Creepers-Fire">https://github.com/coolsquid/Creepers-Fire</a></p>
-				<p><span class="link">CurseForge:</span> <a href="https://minecraft.curseforge.com/projects/creepers-fire">https://minecraft.curseforge.com/projects/creepers-fire</a></p>
+			<h2>Creepers Fire</h2>
+			<p>Creepers Fire is a mod that makes creepers explode when set on fire. It was originally created by strikingwolf for Minecraft 1.7.10, and updated to later versions by me.</p>
+			<p><span class="link">GitHub:</span> <a href="https://github.com/coolsquid/Creepers-Fire">https://github.com/coolsquid/Creepers-Fire</a></p>
+			<p><span class="link">CurseForge:</span> <a href="https://minecraft.curseforge.com/projects/creepers-fire">https://minecraft.curseforge.com/projects/creepers-fire</a></p>
 
-				<h2>Shared Resource Packs</h2>
-				<p>Shared Resource Pack allows the user to share resource packs between multiple Minecraft instances, without storing them in separate directories for each instance.</p>
-				<p><span class="link">GitHub:</span> <a href="https://github.com/coolsquid/Shared-Resource-Packs">https://github.com/coolsquid/Shared-Resource-Packs</a></p>
-				<p><span class="link">CurseForge:</span> <a href="https://minecraft.curseforge.com/projects/shared-resource-packs">https://minecraft.curseforge.com/projects/shared-resource-packs</a></p>
+			<h2>Shared Resource Packs</h2>
+			<p>Shared Resource Pack allows the user to share resource packs between multiple Minecraft instances, without storing them in separate directories for each instance.</p>
+			<p><span class="link">GitHub:</span> <a href="https://github.com/coolsquid/Shared-Resource-Packs">https://github.com/coolsquid/Shared-Resource-Packs</a></p>
+			<p><span class="link">CurseForge:</span> <a href="https://minecraft.curseforge.com/projects/shared-resource-packs">https://minecraft.curseforge.com/projects/shared-resource-packs</a></p>
 
-				<br>
-				<p>The mods listed above are dedicated to the public domain, and may be distributed as part of any modpack.</p>
+			<br>
+			<p>The mods listed above are dedicated to the public domain, and may be distributed as part of any modpack.</p>
 		</div>
 	</body>
 </html>
